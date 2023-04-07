982d2764b1dfe719e18334f94f7881f130ba7577
Thien Tran <fcduythien@gmail.com>
2023-04-07 21:56:56 +0700

    add example github action

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[test]
+fn it_works() {
+    assert_eq!(2 + 2, 4);
+}
+
+#[test]
+#[ignore]
+fn expensive_test() {
+    assert_eq!(2 + 5, 7);
+    // code that takes an hour to run
+}
